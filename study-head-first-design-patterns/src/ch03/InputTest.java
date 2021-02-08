4710fd55ff3b072ffe7081668809f036207aaec3
thisisnew <thisisnew@naver.com>
2021-02-08 15:52:42 +0900

    데코레이터 패턴 공부

--- study-head-first-design-patterns/src/ch03/InputTest.java
+++ study-head-first-design-patterns/src/ch03/InputTest.java
@@ -5,7 +5,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.io.InputStream;
-
+ 
 public class InputTest {
 	public static void main(String[] args) throws IOException {
 		int c;
